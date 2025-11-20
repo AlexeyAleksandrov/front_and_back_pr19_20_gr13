2af561611cdb65f193c4c889ebc4f98c0f48e2f8
alexeyaleksandrov <chinok73@yandex.ru>
2025-11-20 18:44:55 +0300

    pr_23-24

--- technololy_tracker/src/App.jsx
+++ technololy_tracker/src/App.jsx
@@ -165,7 +165,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/user/:userId/:groupId/:starosta" element={<UserProfile />} />
+            <Route path="/user/:userId" element={<UserProfile />} />
             <Route path="/login" element={<Login onLogin={handleLogin} />} />
             
             {/* защищенный маршрут */}
